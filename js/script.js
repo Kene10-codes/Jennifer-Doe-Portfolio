a91cd8104fbf3356aed92ef4f40e9ea8c0200088
kenenwobodo <nwobodokenechukwu2@gmail.com>
2020-08-06 22:16:33 +0100

    portfolio update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+showNavBar = () => {
+     let topNav = document.getElementById("myTopNav");
+    
+    if (topNav.className === "topNavBar") {
+        topNav.className += " responsive";
+
+    } else {
+        topNav.className = "topNavBar";
+    }
+}

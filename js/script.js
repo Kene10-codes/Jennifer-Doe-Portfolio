94c6c484b2b082303c66f4e42821e2bd2ad92f97
tundeLayan <tunderoxyafolayan@gmail.com>
2020-08-19 12:51:12 +0100

    feat:Alert comes up on successful form submission

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
+let errors = {};
+let flag;
+
 showNavBar = () => {
-     let topNav = document.getElementById("myTopNav");
-    
+    let topNav = document.getElementById("myTopNav");
     if (topNav.className === "topNavBar") {
         topNav.className += " responsive";
-
     } else {
         topNav.className = "topNavBar";
     }
 }
-
 
 const form = document.getElementById("form");
 const fullName = document.getElementById("fullName");
@@ -16,15 +16,19 @@ const email = document.getElementById("email");
 const subject = document.getElementById("subject");
 const comment = document.getElementById("comment");
 
-
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
-
     checkFormInputs();
-
+    flag = isEmpty(errors);
+    if(flag){
+        alert('Sent')
+    }else{
+        alert('Not Sent')
+    }
 })
 
 checkFormInputs = () => {
+    errors = {};
     const fullNameValue = fullName.value.trim();
     const emailValue = email.value.trim();
     const subjectValue = subject.value.trim();
@@ -34,47 +38,43 @@ checkFormInputs = () => {
     //add error message
     if(fullNameValue == "") {
         setErrorFor(fullName, "Name cannot be empty");
+        error.fullName = "Input fullName";
     } else {
         setSuccessFor(fullName);
     }
 
     if(emailValue == "") {
         setErrorFor(email, "Email cannot be empty");
-
+        error.fullName = "Input Email";
     } else if (!isEmail(emailValue)) {
         setErrorFor(email, "Email is not valid!");
-
+        error.fullName = "Input valid email";
     } else {
         setSuccessFor(email)
     }
 
     if(subjectValue == "") {
         setErrorFor(subject, "Subject cannot be empty");
-
+        error.fullName = "Input Subject";
     } else {
         setSuccessFor(subject);
     } 
 
     if(commentValue == "") {
         setErrorFor(comment, "Comment section cannot be empty");
+        error.fullName = "Input Comment";
     } else {
         setSuccessFor(comment);
     }
-
     //show popup
-    
-
-   
 }
 
 setErrorFor = (input, message) => {
     const formControl = input.parentElement; //.form-control
     const small = formControl.querySelector("small");
-
     //add error message inside smaa
     small.innerHTML = message;
     formControl.className = "form-control error";
-
 }
 
 setSuccessFor = (input) => {
@@ -86,4 +86,13 @@ setSuccessFor = (input) => {
 //check correct format
 isEmail = (email) => {
     return  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+// Function to check if object is empty
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
 }
